95e45982ce165e4e08dbc53475b7bacdb7cd0d28
RupdeepSaha <rupdeep176@hotmail.com>
2020-11-25 22:36:08 +0530

    plsql update #2

--- plsql.sql
+++ plsql.sql
@@ -4,20 +4,24 @@ declare
 	phone_number number(10);
 	username varchar2(20);
 	password varchar2(20);
-	password_table varchar2(20);
-	username_table varchar2(20);
 begin
+	--enter custom strings like "Enter name" , etc. for every following input
 	name := '&name';
 	address := '&address';
-	phone_number := '&phone_number';
+	phone_number := &phone_number;
 	username := '&username';
 	password := '&password';
-	select username,password into username_table, password_table
-	from customer
-	while username = username_table loop
-		username := '&username';
-	while password = password_table loop
-		password := '&password';
+	for t in (select username,phone_number from customer) loop
+	 while username = t.username loop
+	 	--insert custom string "Username is taken plz enter different username"
+	 	username := '&username';
+	 end loop;
+	 while phone_number = t.phone_number loop
+	 	--insert custom string "Phone Number is taken plz enter different phone number"
+	 	password := &phone_number;
+	 end loop;
+	end loop;
+	--insert into table customer the new values and print successfully registered
 end;
 /
 
