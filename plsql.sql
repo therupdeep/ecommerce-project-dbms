0e011d94dbd57604ce01582c40a53ceea8c35bdd
RupdeepSaha <rupdeep176@hotmail.com>
2020-11-26 23:58:55 +0530

    register procedure working

--- plsql.sql
+++ plsql.sql
@@ -1,27 +1,24 @@
-declare 
-	name varchar2(20);
-	address varchar2(20);
-	phone_number number(10);
-	username varchar2(20);
-	password varchar2(20);
+create or replace procedure register(
+	name varchar2,
+	address varchar2,
+	phone_number number,
+	username varchar2,
+	password varchar2
+) 
+is
 begin
-	--enter custom strings like "Enter name" , etc. for every following input
-	name := '&name';
-	address := '&address';
-	phone_number := &phone_number;
-	username := '&username';
-	password := '&password';
 	for t in (select username,phone_number from customer) loop
 	 while username = t.username loop
-	 	--insert custom string "Username is taken plz enter different username"
-	 	username := '&username';
+	 	dbms_output.put_line('Enter different Username. This username already exists');
+	 	return;
 	 end loop;
 	 while phone_number = t.phone_number loop
-	 	--insert custom string "Phone Number is taken plz enter different phone number"
-	 	phone_number := &phone_number;
+	 	dbms_output.put_line('Enter different Phone Number. This phone number is taken');
+	 	return;
 	 end loop;
 	end loop;
-	--insert into table customer the new values and print successfully registered
+	--set customer id and cart id here
+	--insert values into customer table
+	dbms_output.put_line('You are successfully registered');
 end;
 /
-
