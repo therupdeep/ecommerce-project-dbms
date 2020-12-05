bcd8e60caf6f87262ebdf66a35526033d0b0cfa0
Susnato Barua <mantumantu007@gmail.com>
2020-12-05 21:32:50 +0530

    Update addmoney.sql

--- executable_files/addmoney.sql
+++ executable_files/addmoney.sql
@@ -1,10 +1,12 @@
 set serveroutput on;
+accept money customer.wallet%type prompt 'Enter the amount you want to add in your wallet';
 declare
 		money customer.wallet%type;
 		username customer.username%type;
 begin
    		money:='&money';
 		username:='&username';
+		show_balance(username);
    		addmoney(money,username);
 end;
 /
