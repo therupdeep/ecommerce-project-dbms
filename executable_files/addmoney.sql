ddd291598b525ad9c7aeb89329eae25621038c87
Susnato Barua <mantumantu007@gmail.com>
2020-12-05 21:27:13 +0530

    Update addmoney.sql

--- executable_files/addmoney.sql
+++ executable_files/addmoney.sql
@@ -1,8 +1,10 @@
 set serveroutput on;
 declare
 		money customer.wallet%type;
+		username customer.username%type;
 begin
    		money:='&money';
+		username:='&username';
    		addmoney(money,username);
 end;
 /
