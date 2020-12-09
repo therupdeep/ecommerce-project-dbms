d403ac156e2318f1b0168d8f898fc4b216095880
RajdeepDey010 <rdey5486@gmail.com>
2020-12-09 23:57:09 +0530

    queries added

---
+++ queries/queries.sql
@@ -0,0 +1,8 @@
+select * from Product where price<1000 and category_id='ctg7';
+
+select * from Product where price<1000;
+
+select * from Product where brand='Nestle';
+
+select * from Product where price<40000 and category_id='ctg1';
+
