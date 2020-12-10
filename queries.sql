3a252cb86bc0f68c307441709f06bdf31da6ba6b
SusnatoPrime007 <mantumantu007@gmail.com>
2020-12-10 20:34:00 +0530

    Added 1st query

--- queries.sql
+++ queries.sql
@@ -1,3 +1,8 @@
+@ /home/susnato/Desktop/ecommerce-project-dbms/exec_files/add_to_cart.sql;
+select * from cart_item;
+select * from cart;
+
+
 select * from Product where price<1000 and category_id='ctg7';
 
 select * from Product where price<1000;
