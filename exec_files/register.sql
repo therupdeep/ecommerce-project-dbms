2dc24a5bda4b1d60d96af90845a3bb5a7ea6456d
Susnato Barua <mantumantu007@gmail.com>
2021-01-12 20:34:05 +0530

    Added email id

--- exec_files/register.sql
+++ exec_files/register.sql
@@ -3,15 +3,17 @@ set linesize 3000;
 declare 
 	Name customer.name%type;
 	Address customer.address%type;
+	Email_id customer.email_id%type;
 	Phone_Number customer.phone_number%type;
 	Username customer.username%type;
 	Password customer.password%type;
 begin
 	Name := '&Name';
 	Address := '&Address';
+	Email_id := '&Email_id';
 	Phone_Number := '&Phone_Number';
 	Username := '&Username';
 	Password := '&Password';
-	register(Name,Address,Phone_Number,Username,Password);
+	register(Name,Address,Email_id,Phone_Number,Username,Password);
 end;
 /
