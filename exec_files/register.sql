960c0fc577f5d3c9b9bafe4008240e053a3973ab
rajdeeptechky <55846486+RajdeepDey010@users.noreply.github.com>
2021-01-12 20:45:49 +0530

    removed username

--- exec_files/register.sql
+++ exec_files/register.sql
@@ -5,7 +5,6 @@ declare
 	Address customer.address%type;
 	Email_id customer.email_id%type;
 	Phone_Number customer.phone_number%type;
-	Username customer.username%type;
 	Password customer.password%type;
 begin
 	Name := '&Name';
@@ -14,6 +13,6 @@ begin
 	Phone_Number := '&Phone_Number';
 	Username := '&Username';
 	Password := '&Password';
-	register(Name,Address,Email_id,Phone_Number,Username,Password);
+	register(Name,Address,Email_id,Phone_Number,Password);
 end;
 /
